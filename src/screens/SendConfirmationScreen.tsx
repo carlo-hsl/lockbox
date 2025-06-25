85eb7aa13be6f0698fba53cbd91af60830776994
Carlo <carlo@pc-5-1-100-190.cm.vtr.net>
2025-06-25 15:07:30 +0800

    Add QR code to transaction summary for receiver scan

--- src/screens/SendConfirmationScreen.tsx
+++ src/screens/SendConfirmationScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import QRCode from "qrcode.react";
 
 const SendConfirmationScreen: React.FC = () => {
   const navigate = useNavigate();
@@ -93,6 +94,10 @@ const SendConfirmationScreen: React.FC = () => {
           >
             {copied ? 'Copied!' : 'Copy'}
           </button>
+        </div>
+        <div style={{ marginTop: 20, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <div style={{ color: '#999', fontSize: 13, marginBottom: 8 }}>Or scan QR code:</div>
+          <QRCode value={shareUrl} size={120} bgColor="#2d2d2d" fgColor="#4a9eff" includeMargin={true} />
         </div>
       </div>
       
